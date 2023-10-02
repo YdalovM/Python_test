c26b36e036dd0be11b938f18006665e25fc4ed81
YdalovM <144206674+YdalovM@users.noreply.github.com>
2023-10-02 13:51:31 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,59 @@
+# первая задача print((a := int(input())), '\n', a + 1, '\n', a + 2, sep = '')
+
+# вторая задача print(int(input()) + int(input()) + int(input()))
+
+# третья задача print('Объем = ', (a := int(input())**3), '\n', 'Площадь полной поверхности = ', 6*a**2, sep = '')
+
+# четвертая задача print(3*((a := int(input()))*(b := int(input())))**3 +275*b**2-127*a - 41)
+
+# пятая задача print('Следующие за числом ', (num := int(input())), ' ', 'число: ', num + 1, '\n', 'Для числа ', num, ' ', 'предыдущее число: ', num - 1, sep = '')
+
+# шестая задача print(((int(input())) + (int(input())) + (int(input()))) * 3)
+
+# седьмая задача print(int(input()) + int(input()) *(int(input()) - 1))
+
+# восьмая задача print(a := int(input()), (a*2), (a*3), (a*4), (a*5), sep = '---')
+
+# девятая задача a, b = int(input()), int(input()); print((b // a), (b % a)) в одну строку не совсем получилось(
+
+# десятая задача print((int(input())+1)//2)
+
+# одинадцатая задача print(a := int(input()), 'мин - это', a // 60, 'час', a % 60, 'минут.')
+
+# двенадцать - надоело
+# num = int(input())
+# a = num % 10
+# b = (num % 100) // 10
+# c = num // 100
+# print('Произведение цифр = ', a + b + c, '\n','Сумма цифр = ', a * b * c, sep = '')
+
+# тренадцать
+# v = int(input())
+# a1 = '    _~_    '; a2 = '   (o o)   '; a3 = '  /  V  \  '; a4 = ' /(  _  )\ '; a5 = '   ^^ ^^   '
+# print(a1*v)
+# print(a2*v)
+# print(a3*v)
+# print(a4*v)
+# print(a5*v)
+
+# четырнадцать
+# num = int(input())
+# b = (num % 100) // 10
+# print(b)
+
+# петнадцать
+# a = int(input())
+# b = a // 60 % 24
+# c = a % 60
+# print(b, c,)
+
+# шестнадцать
+# print((int(input()) + 1) % 2)
+
+# семнадцать
+# a = int(input()); b = int(input())
+# print(a * b - 109)
+
+# восемнадцать
+#h = int(input()); a = int(input()); b = int(input())
+#print((h - a) // (a - b) + 1)
